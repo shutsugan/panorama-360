6f659fec3509facb3b4ab72213bbae4084354993
Muhammad Tajjiou <shutsugan@gmail.com>
2018-08-30 09:50:35 +0100

    Added a sphere to the screen

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+const geometry = new THREE.SphereGeometry(50, 32, 32);
+const material = new THREE.MeshBasicMaterial({color: 0xFFFF00});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
