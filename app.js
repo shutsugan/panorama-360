c752b6a9e23cc9d6311e724f595b94d4322ffa92
Muhammad Tajjiou <shutsugan@gmail.com>
2018-08-30 09:46:36 +0100

    Added a style file, used the script in the main html file, and cleaned the app file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
